44a3c5110a39fcdf4565d98db3c8a4975c299011
TristanEPSI <tristan.gottschak@laposte.net>
2023-12-04 15:51:15 +0100

    Clean process and root structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,48 @@
 import React from 'react';
+import Map from './composants/Cartes/map';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import EventList from './composants/EventList';
-import EventForm from './composants/EventForm';
-import ArtistList from './composants/ArtistList';
-import ArtistForm from './composants/ArtistForm';
+import EventList from './composants/Formulaires/EventList';
+import EventForm from './composants/Formulaires/EventForm';
 
 const App = () => {
   return (
     <Router>
-      <ul>
-        <li><Link to="/events">Événements</Link></li>
-        <li><Link to="/artists">Artistes</Link></li>
-      </ul>
+      <div className='flex flex-col h-[100VH]'>
+
+        <header id='header-section' className='w-full h-[8%] fixed items-center flex justify-between p-3'>
+          <div>
+            <img alt='Logo App'></img>
+          </div>
+
+          <div>
+            Nation-sounds
+          </div>
+
+          <ul>
+            <li><Link to="/events">Événements</Link></li>
+          </ul>
+
+        </header>
+
+        <div id='main-content' className='w-full h-full flex flex-col md:flex-row justify-around items-center'>
+          <div className='h-[200px] w-[80%] md:w-[50%] md:h-[80%] text-center border-red-500 border-2'> Map </div>
+          <Map></Map>
+          <div className='h-[100px]'> Event </div>
+          <div className='h-[100px]'> Info </div>
+
+        </div>
+
+
+        <footer id='footer-section' className='w-full h-[10%] flex justify-between items-center p-3 mt-auto'>
+          <div id='contact'> Contacts </div>
+          <div id='partenaire-footer'> Partenaires </div>
+        </footer>
+      </div>
+
+
       <Routes>
         <Route path="/events" element={<EventList />} />
         <Route path="/events/add" element={<EventForm />} />
-        <Route path="/artists" element={<ArtistList />} />
-        <Route path="/artists/add" element={<ArtistForm />} />
       </Routes>
     </Router>
   );
