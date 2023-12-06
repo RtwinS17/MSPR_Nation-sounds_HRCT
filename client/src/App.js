fcd60e1873e90b26419254d1807bffa60d123b02
TristanEPSI <tristan.gottschak@laposte.net>
2023-12-06 11:35:54 +0100

    Cleaning base and test for variables css with @

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import Map from './composants/Cartes/map';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import EventList from './composants/Formulaires/EventList';
-import EventForm from './composants/Formulaires/EventForm';
 
 const App = () => {
   return (
-    <Router>
+   
       <div className='flex flex-col h-[100VH]'>
 
         <header id='header-section' className='w-full h-[8%] fixed items-center flex justify-between p-3'>
@@ -19,14 +16,13 @@ const App = () => {
           </div>
 
           <ul>
-            <li><Link to="/events">Événements</Link></li>
+            <li>Events</li>
           </ul>
 
         </header>
 
-        <div id='main-content' className='w-full h-full flex flex-col md:flex-row justify-around items-center'>
-          <div className='h-[200px] w-[80%] md:w-[50%] md:h-[80%] text-center border-red-500 border-2'> Map </div>
-          <Map></Map>
+        <div id='main-content' className='w-full h-full flex flex-col md:flex-row justify-around items-center pt-6'>
+          <div className='h-[200px] w-[80%] md:w-[50%] md:h-[80%] text-center'> <Map></Map> </div>
           <div className='h-[100px]'> Event </div>
           <div className='h-[100px]'> Info </div>
 
@@ -39,12 +35,6 @@ const App = () => {
         </footer>
       </div>
 
-
-      <Routes>
-        <Route path="/events" element={<EventList />} />
-        <Route path="/events/add" element={<EventForm />} />
-      </Routes>
-    </Router>
   );
 };
 
