b6f932a57c685a8f1d5cbbde2a047825c9353fcf
cristelleal <cristelleal@gmail.com>
2023-12-19 17:47:57 +0100

    feat: add geolocation to Map component

--- client/src/composants/Map/map.jsx
+++ client/src/composants/Map/map.jsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import './map.style.css';
 import L from 'leaflet';
 import customIconImg from './location-icon.svg';
 
 const Map = () => {
-  const mapCenter = [48.8340, 2.4510];
+  const mapCenter = [48.834, 2.451];
   const mapZoom = 13;
 
   const customIcon = new L.Icon({
     iconUrl: customIconImg,
-    iconSize: [32, 32], 
-    iconAnchor: [16, 32], 
-    popupAnchor: [0, -32], 
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
   });
+
+  function LocationMarker() {
+    const [position, setPosition] = useState(null);
+
+    const map = useMap();
+
+    useEffect(() => {
+      map.locate().on('locationfound', function (e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+      });
+    }, [map]);
+
+    return position === null ? null : (
+      <Marker position={position} icon={customIcon}>
+        <Popup>Vous êtes ici</Popup>
+      </Marker>
+    );
+  }
 
   const festivalLocations = [
     { id: 1, title: 'Scène principale', coordinates: [48.8325, 2.4492] },
-    { id: 2, title: 'Scène A', coordinates: [48.8340, 2.4510] },
-    { id: 3, title: 'Scène B', coordinates: [48.8310, 2.4525] },
+    { id: 2, title: 'Scène A', coordinates: [48.834, 2.451] },
+    { id: 3, title: 'Scène B', coordinates: [48.831, 2.4525] },
     { id: 4, title: 'Scène C', coordinates: [48.8305, 2.4485] },
-    { id: 5, title: 'Scène D', coordinates: [48.8345, 2.4470] },
+    { id: 5, title: 'Scène D', coordinates: [48.8345, 2.447] },
   ];
 
   return (
@@ -29,11 +48,12 @@ const Map = () => {
         attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-     {festivalLocations.map((location, index) => (
-      <Marker key={index} position={location.coordinates} icon={customIcon}>
-        <Popup>{location.title}</Popup>
-      </Marker>
-    ))}
+      {festivalLocations.map((location, index) => (
+        <Marker key={index} position={location.coordinates} icon={customIcon}>
+          <Popup>{location.title}</Popup>
+        </Marker>
+      ))}
+      <LocationMarker />
     </MapContainer>
   );
 };
