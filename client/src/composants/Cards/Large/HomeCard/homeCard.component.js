e4c0e31e50e1d0c32c0b3da35148d9508b5a32f0
Remi Pouille <remi.pouille59@gmail.com>
2023-12-21 15:30:35 +0100

    add img

--- client/src/composants/Cards/Large/HomeCard/homeCard.component.js
+++ client/src/composants/Cards/Large/HomeCard/homeCard.component.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./homeCard.style.css";
 import "./../../cards.style.css";
+import imageSrc from '../../../../ressources/AdobeStock_115209041.jpeg';
+
+
 
 export const HomeCard = () => {
     return (
-        <>
-    
-        </>
+        <div class="ml-32 w-full">
+           <img src={imageSrc}alt="putain d'image"></img>
+            
+        </div>
+        
     )
 }
