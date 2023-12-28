76be744e49ee715a6ad13071b1114f6726f10261
TristanEPSI <tristan.gottschak@laposte.net>
2023-12-28 16:42:12 +0100

    update: Modification on store, adding theme to the store, adding svg for theme mode, feature : theme mode is now available, installation of Material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBarMobile from './composants/NavBar_Components/NavBar_Mobile/navBarMobile.component';
 import { NavBarDesktop } from './composants/NavBar_Components/NavBar_Desktop/navBarDesktop.component';
 import { HomeCard } from './composants/Cards/Large/HomeCard/homeCard.component';
-const App = () => {
-  return (
+import { useSelector } from 'react-redux';
+import { ThemeBtn } from './composants/Molecule/Button/themeChanger.btn';
 
+
+
+const App = () => {
+  let [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  let [deviceUsed, setDeviceUSed] = useState('mobile')
+  function updateScreenWidth() {
+    setScreenWidth(window.innerWidth)
+  }
+  useEffect(() => {
+    window.addEventListener('resize', updateScreenWidth)
+    screenWidth >= 640 ? setDeviceUSed('desktop') : setDeviceUSed('mobile')
+  })
+
+  let theme = useSelector(store => store.theme.value)
+
+
+  return (
     <>
-      <div className='h-screen w-screen'>
-       {deviceUsed === 'mobile' ? <NavBarMobile/> : <NavBarDesktop/>}
+      <div className="h-screen w-screen">
+        {deviceUsed === 'mobile' ? <NavBarMobile /> : <NavBarDesktop />}
         <div className='main-container fixed ml-6 h-screen flex flex-col mt-12 sm:ml-[100px]'>
           <HomeCard />
+          <ThemeBtn />
         </div>
       </div>
     </>
-
   );
 };
 
 export default App;
-
-// Importance de récupurer la taille de l'écran à minima au démarrage de l'app
-
-let deviceUsed = '';
-
-export const getDeviceScreenSize = () => {
-  let scWidth = window.innerWidth;
-if (scWidth) {
-  if (+scWidth <= 640) {
-    deviceUsed = 'mobile'
-  } else deviceUsed = 'desktop'
-} else return 'screen width undected'
-}
-
-// State
-
-// On init
-getDeviceScreenSize()
--- client/src/composants/Molecule/Icon/burgerIcon-component.js
+++ client/src/composants/Molecule/Icon/burgerIcon-component.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import burgerIcon from './../../../ressources/burger-menu-svgrepo-com.svg';
+import burgerIconNight from './../../../ressources/burgerIcon_night.svg';
 import './icons.style.css';
 
-
 function BurgerIcon({id=""}) {
-  // Import result is the URL of your image
+  let theme = useSelector(store => store.theme.value)
+
   return (
     <div id={id} className='burger-icon w-[50px]'>
-      <img src={burgerIcon} alt="Icon for menu" />
+     {theme === "day" ? <img src={burgerIcon} alt="Icon for menu" /> : <img src={burgerIconNight} alt="Icon for menu" />} 
     </div>
   )
 }
