a258e12914b304d0f7dfbcdcbf790e644163411a
Tristan Gottschalk <123099307+TristanEPSI@users.noreply.github.com>
2024-02-07 12:49:40 +0100

    First step into middleware, actions & redcuers iniated

--- client/src/appStore/AppState/InitialState.state.js
+++ client/src/appStore/AppState/InitialState.state.js
@@ -1,90 +1,36 @@
-const initialState = {
-    Alertes : [{
-        idAlerte: 0,
-        title: "",
-        type: "",
-        texte: "",
-        createAt: "",
-        idUser: 0,
-        actif: false, // !
-    },],
-
-    Commentaires :[{
-        idCommentaire: 0,
-        idUser: 0,
-        idConcert: 0,
-        texte: "",
-        note: 0,
-        date: "",
-        createAt: "",
-        reply: [Map(id, comments)] // ! Map des commentaires en réponse au commentaire
-    },],
-
-    Concerts: [{
-        idConcert: 0,
-        groupe: "",
-        horaire: "",
-        scene: "",
-        description: "",
-        lieu: "",
-        genre: "",
-        partenaire: [],
-    },],
-
-    Lieux: [{
-        idLieu: 0,
-        alias: "", // ! 
-        localisation: {
-            latitude: 0,
-            longitutude: 0,
-        },
-        concerts: [idConcert],
-        genre: [],
-        POIS: [],
-    },],
-
-    Genres: [{
-        idGenre: 0,
-        name: "",
-        concerts : [idConcert,] // !
-    },],
-
-    Partenaires: [{
-        idPartenaire: 0,
-        categorie: [],
-        name: "",
-        logo: "",
-        URL: "",
-    },],
-
-    POIs: [{
-        idPOI: 0,
-        name: "",
-        type: "",
-        idLieu: 0,
-    },],
-
-    Preferences: [{
-        idPreference: 0,
-        idUser: "",
-        theme: "",
-        genre: [],
-        notifications: [],
-    },],
-
-    Scenes: [{
-        idScene: 0,
-        name: "",
-        type: "",
-        idLieu: 0,
-    },],
-
-    Users: [{
-        idUser: 0,
-        name: "",
-        email: "",
-        password: "",
-        token: "",
-        commentaires: [],
-    },],
+export const initialState = {
+    loading: false,
+    error: null,
+    data: {
+        Alertes : [
+    ],
+    
+        Commentaires :[
+    ],
+    
+        Concerts: [
+    ],
+    
+        Lieux: [
+    ],
+    
+        Genres: [
+    ],
+    
+        Partenaires: [
+    ],
+    
+        POIs: [
+    ],
+    
+        Preferences: [
+    ],
+    
+        Scenes: [
+    ],
+    
+        Users: [
+    ],
+    }
+    
 }
--- client/src/appStore/actions/actions Service.js
+++ client/src/appStore/actions/actions Service.js
@@ -2,3 +2,27 @@
 export function createAction (type = "DEFAULT ACTION", props = null){
     return 'ACTION_' + type
 }
+
+export const getInitialState = {
+ getInitialState() {
+    return ({
+        type: 'ACTION_INITIALIZE_APP',
+    })
+ },
+
+ getInitialStateSuccess(data) {
+    return ({
+        type: 'ACTION_SUCCESS_INITIALIZING_APP',
+        payload: data
+    })
+ },
+
+ getInitialStateFailed(errorMessage){
+    return ({
+        type: 'ACTION_FAILED_INITIALIZING_APP',
+        payload: errorMessage
+    })
+ }
+}
+
+
