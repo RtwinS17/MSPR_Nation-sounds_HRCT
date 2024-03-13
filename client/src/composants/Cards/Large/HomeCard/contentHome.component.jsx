22451f976066d49808eadbe7d522c4fa27058ef4
TristanEPSI <tristan.gottschak@laposte.net>
2024-03-13 15:28:47 +0100

    updates: Changes on the home page, adding png to ressources. TODO --> Add the png to the home content

--- client/src/composants/Cards/Large/HomeCard/contentHome.component.jsx
+++ client/src/composants/Cards/Large/HomeCard/contentHome.component.jsx
@@ -4,19 +4,27 @@ import InteractiveMap from "../../../WordPress/InteractiveMap";
 
 import './contentHome.style.css';
 
+const textDiscover = {
+    Discover : "Découvrez Nations Sounds, le festival de musique incontournable de Paris ! Plongez dans une expérience musicale unique au cœur de la Ville Lumière",
+
+    Programmation: "Découvrez la diversité musicale de Nations Sounds ! Des artistes de tous les horizons se réunissent pour vous offrir une expérience inoubliable. Consultez notre programmation maintenant ! ",
+    
+    Billetterie: "La billetterie pour Nations Sounds est ouverte jusqu'au 30/07/2026 ! Ne manquez pas l'opportunité de vivre une expérience musicale exceptionnelle au cœur de Paris."
+}
+
 export const HomeContent = () => {
 
     return (
         <>
-            <div className="h-[90%] flex flex-col gap-3 homeContainer p-3">
+            <div className="h-[90%] flex flex-col gap-3 p-3">
                 <article className="basis-2/6">
-                    <DiscoverCards title=" DÉCOUVRIR LE FESTIVAL"/>
+                    <DiscoverCards title=" DÉCOUVRIR LE FESTIVAL" text={textDiscover.Discover} linkTo="/infos-pratiques" imageSrc="./../../../../ressources/discover"/>
                 </article>
                 <article className="basis-2/6">
-                    <DiscoverCards title="PROGRAMMATION" linkText="Découvrir les artistes"/>
+                    <DiscoverCards title="PROGRAMMATION" text={textDiscover.Programmation} linkText="Découvrir les artistes" linkTo="/programmation" imageSrc="./../../../../ressources/programmation"/>
                 </article>
                 <article className="basis-2/6">
-                    <DiscoverCards title="BILLETTERIE" linkText="Acheter un billet"/>
+                    <DiscoverCards title="BILLETTERIE" text={textDiscover.Billetterie} linkText="Acheter un billet" linkTo="/billeterie" imageSrc="./../../../../ressources/billetterie"/>
                 </article>
             </div>
             <div className="hidden sm:flex h-[10%] w-full justify-center">
--- client/src/composants/Cards/Large/HomeCard/homeCard.component.js
+++ client/src/composants/Cards/Large/HomeCard/homeCard.component.js
@@ -7,7 +7,7 @@ import "./../../cards.style.css";
 
 export const MainContent = ({children=""}) => {
     return (
-        <main className="w-screen h-screen flex justify-center align-middle">
+        <main className="w-screen h-screen flex justify-center align-middle homeContainer">
             <div className="mainContent-container p-3 rounded-xl flex flex-col justify-center">
             {children}
             </div>
