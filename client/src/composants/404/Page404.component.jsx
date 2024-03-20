b81d97b35bece30007e74090efadee814fca82bb
TristanEPSI <tristan.gottschak@laposte.net>
2024-03-20 16:18:53 +0100

    feature: Adding page 404

--- client/src/composants/404/Page404.component.jsx
+++ client/src/composants/404/Page404.component.jsx
@@ -3,11 +3,16 @@ import { Link } from "react-router-dom"
 
 import './Page404.style.css'
 
+import { MainContent } from "../Permanent/homeCard.component"
+import { Link } from "react-router-dom"
+
+import './Page404.style.css'
+
 export const Page404 = () => {
     return (
         <MainContent>
             <div className="flex flex-col h-full justify-around items-center svg404">
-                <h1 className="text-4xl"> Erreur 404 </h1>
+                <h1 className="text-4xl p-4 rounded-lg decoration-2 underline"> 404 </h1>
                 <span className="h-32 flex items-center p-8 rounded-lg container404"> La page que vous demandez n'existe pas ! </span>
                 <Link to='..' className="btn-secondary p-4"> Revenir à l'accueil </Link>
             </div>
