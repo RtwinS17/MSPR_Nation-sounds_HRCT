2a67df54b18b97daf9cb4e4786234869160005e0
TristanEPSI <tristan.gottschak@laposte.net>
2024-04-30 15:15:24 +0200

    Adding alertes component

--- client/src/composants/Info-pratiques/Alertes/Alertes.component.jsx
+++ client/src/composants/Info-pratiques/Alertes/Alertes.component.jsx
@@ -1,6 +1,12 @@
+import { MainContent } from "../../Permanent/homeCard.component"
+import AlertesWP from "../../WordPress/AlertesWP"
+
 export const Alertes = () => {
     return (
-        <>
-        </>
+     <MainContent>
+        <AlertesWP>
+            
+        </AlertesWP>
+     </MainContent>
     )
 }
--- client/src/composants/WordPress/AlertesWP.jsx
+++ client/src/composants/WordPress/AlertesWP.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import './AlertesWP.style.css';
+
 const AlertesWP = () => {
   const [alertes, setAlertes] = useState([]);
 
   useEffect(() => {
     const fetchAlertes = async () => {
       try {
-        const response = await axios.get('https://nationsounds.online/wp-json/wp/v2/posts?categories=3');
+        const response = await axios.get('https://nationsounds.online/wp-json/wp/v2/posts?categories=5');
         setAlertes(response.data);
       } catch (error) {
         console.error(error);
@@ -18,11 +20,13 @@ const AlertesWP = () => {
   }, []);
 
   return (
-    <div>
-      <h2>Alertes</h2>
-      <ul>
+    <div className='h-full overflow-y-auto rounded-b-md alertes-container'>
+      <h2 className='text-center p-8'>Alertes</h2>
+      <ul className='flex flex-col gap-8 my-8 overflow-y-scroll'>
         {alertes.map(alerte => (
-          <li key={alerte.id}>{alerte.title.rendered}</li>
+          <div className='alerte text-center pb-4 mx-4'>
+          <li className='p-4 w-[80%] md:w-[60%] m-auto rounded-3xl' key={alerte.id}>{alerte.title.rendered}</li>
+          </div>
         ))}
       </ul>
     </div>
