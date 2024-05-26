cfe1c5c2132d497e2808b3d29fca34afbfa6ff2f
Hamdane Khiari <76649362+khiari-h@users.noreply.github.com>
2024-05-26 10:29:30 +0200

    feat:Update Interactive Map component

--- client/src/composants/WordPress/InteractiveMap.jsx
+++ client/src/composants/WordPress/InteractiveMap.jsx
@@ -1,29 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 const InteractiveMap = () => {
-    const [mapContent, setMapContent] = useState('');
-
-    useEffect(() => {
-        axios.get('https://nationsounds.online/carte-du-concert/')
-            .then(response => {
-                const pageContent = response.data;
-                const regex = /\[google_map_easy id="(\d+)"\]/g;
-                const match = regex.exec(pageContent);
-                if (match && match[1]) {
-                    setMapContent(`<iframe src="https://www.google.com/maps/d/embed?mid=${match[1]}" width="640" height="480"></iframe>`);
-                }
-            })
-            .catch(error => { 
-
-                console.error('Erreur lors de la écupération du contenu de la carte :', error);
-            });
-    }, []);
-
     return (
         <div>
             <h1>Carte interactive du concert</h1>
-            <div dangerouslySetInnerHTML={{ __html: mapContent }} />
+            <iframe 
+                src="https://nationsounds.online/carte-du-concert/"
+                width="100%"
+                height="600"
+                style={{ border: 'none' }}
+                title="Carte Interactive du Concert"
+            ></iframe>
         </div>
     );
 };
