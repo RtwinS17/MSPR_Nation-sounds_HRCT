241cd5a400ea72f9b4bd13b8d78fbd18a0860b7b
RtwinS17 <123241300+RtwinS17@users.noreply.github.com>
2024-07-21 11:33:26 +0200

    Archi project (#55)
    * Update : ArtistCard component + css
    Update : Artist Table + Controller + Seeder
    New : Artist List Component
    
    * Update : Artist Card component
    Update : Artist Table + Seeder + Migration + Controller
    Update : App.js
    New : ArtistList Component

--- client/src/composants/Discover/Artistes/artistCard.component.js
+++ client/src/composants/Discover/Artistes/artistCard.component.js
@@ -1,39 +1,22 @@
-import React from "react";
-import './artistCard.style.css'
-import ArtistesCard from "./../../../ressources/ArtistesCard.jpeg";
+import React from 'react';
+import './artistCard.style.css'; 
 
-export const ArtistCard = () => {
-    return (
+const ArtistCard = ({ artist }) => {
+  if (!artist) {
+    return null;
+  }
 
-    <div>
-       <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
-        <div className="relative bg-white h-10">
-            <h1 className="text-xl text-center uppercase font-bold"> Programmation 2024 </h1>
-            </div>
-  
-    <div className="mx-2 rounded-lg overflow-hidden shadow-lg transition duration-300 ease-in-out hover:shadow-2xl">
-      <div className="relative">
-        <img className="w-full" src={ArtistesCard} alt="Profile" />
-        <div className="absolute inset-0 transition-opacity duration-300 ease-in-out hover:bg-opacity-50 bg-gray-900 bg-opacity-10">
-          <div className="flex h-full items-end">
-            <div className="bg-gray-900 bg-opacity-50 w-full">
-              <div className="artistes-card-title uppercase font-bold text-xl mb-2  px-4 pt-2">Barry Scott</div>
-              <p className="artistes-card-name text-base px-4 pb-2">
-                Rock
-              </p>
-              <div className="artistes-card-gradient h-2 "></div>
-            </div>
-          </div>
-        </div>
+  return (
+    <div className="card mx-4 my-4 rounded-lg shadow-lg overflow-hidden">
+      <div className="relative h-48 md:h-64">
+      <img src={artist.photo} alt={artist.nom_groupe} className="w-full h-full object-cover object-center" />
       </div>
-      
-    
- 
-</div>
-        
+      <div className="card-body">
+        <h5 className="card-title">{artist.nom_groupe}</h5>
+        <p className="card-text">{artist.type_musique}</p>
+      </div>
     </div>
-    </div>
-    
-)
-}
+  );
+};
+
 export default ArtistCard;
